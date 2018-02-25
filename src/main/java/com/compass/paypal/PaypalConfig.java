67270c50582f13e0b36c8170473f4052424fd40d
lenovo <843635424@qq.com>
2018-02-25 16:16:13 +0800

    密钥初始化

--- src/main/java/com/compass/paypal/PaypalConfig.java
+++ src/main/java/com/compass/paypal/PaypalConfig.java
@@ -9,8 +9,8 @@ import com.paypal.base.rest.PayPalRESTException;
 @Configuration
 public class PaypalConfig {
 	
-	private String CLIENT_ID = "Ab7pri_GacGwZi3s6AqAx0xaWgsV0TCYe05VxqwyXpP1v9u7DMiRJv9bQrwtRVLnbKfKw-d4KzM2NCy_";
-	private String CLIENT_SECRET = "EB-n9sQoM0Ljbf7vF7GnAjQdn6XtBAlQMjnyZekyUY8G9KUjeW8gPa0Bbp7hijgV3paSgIs1R1h6Jszr";
+	private String CLIENT_ID = "*******";
+	private String CLIENT_SECRET = "*******";
 	private String MODE = "sandbox";
 	
     @Bean
